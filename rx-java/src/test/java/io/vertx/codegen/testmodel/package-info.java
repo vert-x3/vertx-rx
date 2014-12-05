5c2b5370efbd8118c8945b79b973e65eba207f1f
Julien Viet <julien@julienviet.com>
2014-12-05 11:38:30 +0100

    Remove vertx-core and vertx-codegen sources

--- rx-java/src/test/java/io/vertx/codegen/testmodel/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
- */
-@GenModule(name = "testmodel")
-package io.vertx.codegen.testmodel;
-
-import io.vertx.codegen.annotations.GenModule;
