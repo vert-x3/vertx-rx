e4a9893a702a5c6b5c921ab6dbe25c883fe5b851
Julien Viet <julien@julienviet.com>
2015-04-23 11:04:20 +0200

    Tweak the internal state to have the status for the ReadStream rather than the subscriber

--- rx-java/src/test/java/io/vertx/rx/java/test/BackPressureTest.java
+++ rx-java/src/test/java/io/vertx/rx/java/test/BackPressureTest.java
@@ -5,11 +5,9 @@ import io.vertx.rx.java.ReadStreamAdapter;
 import io.vertx.rx.java.test.stream.BufferReadStreamImpl;
 import org.junit.Test;
 import rx.Observable;
-import rx.Producer;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
@@ -48,16 +46,10 @@ public class BackPressureTest extends AbstractReadStreamAdapterTest<Buffer> {
       }
     };
     Observable<Buffer> observable = toObservable(stream);
-    AtomicReference<Producer> producer = new AtomicReference<>();
     MySubscriber<Buffer> subscriber = new MySubscriber<Buffer>() {
       @Override
       public void onStart() {
         request(0);
-      }
-      @Override
-      public void setProducer(Producer p) {
-        super.setProducer(p);
-        producer.set(p);
       }
     };
     observable.subscribe(subscriber);
@@ -66,7 +58,7 @@ public class BackPressureTest extends AbstractReadStreamAdapterTest<Buffer> {
     stream.handler.handle(buffer("0"));
     assertEquals(1, paused.get());
     subscriber.assertEmpty();
-    producer.get().request(1);
+    subscriber.getProducer().request(1);
     subscriber.assertItem(buffer("0")).assertEmpty();
     assertEquals(1, paused.get());
   }
@@ -218,5 +210,204 @@ public class BackPressureTest extends AbstractReadStreamAdapterTest<Buffer> {
     assertEquals(Long.MAX_VALUE - 1, adapter.getExpected());
     stream.handler.handle(buffer("0"));
     assertEquals(Long.MAX_VALUE, adapter.getExpected());
+  }
+
+  @Test
+  public void testDeliverEventWhenPaused() {
+    BufferReadStreamImpl stream = new BufferReadStreamImpl() {
+      @Override
+      public BufferReadStreamImpl pause() {
+        return this;
+      }
+    };
+    MySubscriber<Buffer> subscriber = new MySubscriber<Buffer>() {
+      @Override
+      public void onStart() {
+        request(1);
+      }
+    };
+    Observable<Buffer> observable = toObservable(stream);
+    observable.subscribe(subscriber);
+    stream.handler.handle(buffer("0")); // We send an event even though we are paused
+    stream.endHandler.handle(null);
+    subscriber.getProducer().request(1);
+    subscriber.assertItem(buffer("0")).assertCompleted().assertEmpty();
+  }
+
+  @Test
+  public void testEndWhenPaused() {
+    BufferReadStreamImpl stream = new BufferReadStreamImpl() {
+      @Override
+      public BufferReadStreamImpl pause() {
+        return this;
+      }
+    };
+    MySubscriber<Buffer> subscriber = new MySubscriber<Buffer>() {
+      @Override
+      public void onStart() {
+        request(1);
+      }
+    };
+    Observable<Buffer> observable = toObservable(stream);
+    observable.subscribe(subscriber);
+    stream.endHandler.handle(null);
+    subscriber.assertCompleted().assertEmpty();
+  }
+
+  @Test
+  public void testRequestDuringOnNext() {
+    BufferReadStreamImpl stream = new BufferReadStreamImpl();
+    MySubscriber<Buffer> subscriber = new MySubscriber<Buffer>() {
+      @Override
+      public void onStart() {
+        request(1);
+      }
+      @Override
+      public void onNext(Buffer buffer) {
+        super.onNext(buffer);
+        request(1);
+      }
+    };
+    Observable<Buffer> observable = toObservable(stream);
+    observable.subscribe(subscriber);
+    stream.handler.handle(buffer("0"));
+    subscriber.assertItem(buffer("0")).assertEmpty();
+    stream.handler.handle(buffer("1"));
+    subscriber.assertItem(buffer("1")).assertEmpty();
+    stream.handler.handle(buffer("2"));
+    subscriber.assertItem(buffer("2")).assertEmpty();
+    stream.endHandler.handle(null);
+    subscriber.assertCompleted().assertEmpty();
+  }
+
+  @Test
+  public void testDeliverDuringResume() {
+    MySubscriber<Buffer> subscriber = new MySubscriber<Buffer>() {
+      @Override
+      public void onStart() {
+        request(0);
+      }
+    };
+    BufferReadStreamImpl stream = new BufferReadStreamImpl() {
+      @Override
+      public BufferReadStreamImpl pause() {
+        return this;
+      }
+      @Override
+      public BufferReadStreamImpl resume() {
+        handler.handle(buffer("0"));
+        return this;
+      }
+    };
+    Observable<Buffer> observable = toObservable(stream);
+    observable.subscribe(subscriber);
+    subscriber.getProducer().request(1);
+    subscriber.assertItem(buffer("0")).assertEmpty();
+  }
+
+  @Test
+  public void testEndDuringResume() {
+    MySubscriber<Buffer> subscriber = new MySubscriber<Buffer>() {
+      @Override
+      public void onStart() {
+        request(0);
+      }
+    };
+    BufferReadStreamImpl stream = new BufferReadStreamImpl() {
+      @Override
+      public BufferReadStreamImpl pause() {
+        return this;
+      }
+      @Override
+      public BufferReadStreamImpl resume() {
+        endHandler.handle(null);
+        return this;
+      }
+    };
+    Observable<Buffer> observable = toObservable(stream);
+    observable.subscribe(subscriber);
+    subscriber.getProducer().request(1);
+    subscriber.assertCompleted().assertEmpty();
+  }
+
+  @Test
+  public void testBufferDuringResume() {
+    MySubscriber<Buffer> subscriber = new MySubscriber<Buffer>() {
+      @Override
+      public void onStart() {
+        request(0);
+      }
+    };
+    AtomicInteger pauses = new AtomicInteger();
+    BufferReadStreamImpl stream = new BufferReadStreamImpl() {
+      @Override
+      public BufferReadStreamImpl pause() {
+        pauses.incrementAndGet();
+        return this;
+      }
+      @Override
+      public BufferReadStreamImpl resume() {
+        assertEquals(1, pauses.get());
+        handler.handle(buffer("0"));
+        assertEquals(2, pauses.get());
+        handler.handle(buffer("1"));
+        return this;
+      }
+    };
+    Observable<Buffer> observable = toObservable(stream);
+    observable.subscribe(subscriber);
+    subscriber.getProducer().request(1);
+    subscriber.assertItem(buffer("0")).assertEmpty();
+  }
+
+  @Test
+  public void testFoo() {
+    MySubscriber<Buffer> subscriber = new MySubscriber<Buffer>() {
+      @Override
+      public void onStart() {
+        request(0);
+      }
+    };
+    AtomicInteger pauses = new AtomicInteger();
+    BufferReadStreamImpl stream = new BufferReadStreamImpl() {
+      @Override
+      public BufferReadStreamImpl pause() {
+        pauses.incrementAndGet();
+        return this;
+      }
+    };
+    Observable<Buffer> observable = toObservable(stream);
+    observable.subscribe(subscriber);
+    stream.handler.handle(buffer("0"));
+    stream.endHandler.handle(null);
+    subscriber.getProducer().request(1);
+    subscriber.assertItem(buffer("0")).assertCompleted().assertEmpty();
+    assertEquals(1, pauses.get());
+  }
+
+  @Test
+  public void testBar() {
+    MySubscriber<Buffer> subscriber = new MySubscriber<Buffer>() {
+      @Override
+      public void onStart() {
+        request(0);
+      }
+    };
+    AtomicInteger pauses = new AtomicInteger();
+    BufferReadStreamImpl stream = new BufferReadStreamImpl() {
+      @Override
+      public BufferReadStreamImpl pause() {
+        pauses.incrementAndGet();
+        return this;
+      }
+    };
+    Observable<Buffer> observable = toObservable(stream);
+    observable.subscribe(subscriber);
+    stream.handler.handle(buffer("0"));
+    stream.handler.handle(buffer("1"));
+    stream.endHandler.handle(null);
+    subscriber.getProducer().request(1);
+    assertEquals(1, pauses.get());
+    subscriber.assertItem(buffer("0")).assertEmpty();
   }
 }
