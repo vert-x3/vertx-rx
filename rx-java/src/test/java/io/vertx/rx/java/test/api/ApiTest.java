993d6509900998aa7e4a02c5c861c3760d520b13
Julien Viet <julien@julienviet.com>
2016-04-13 14:46:39 +0200

    Minor

--- rx-java/src/test/java/io/vertx/rx/java/test/api/ApiTest.java
+++ rx-java/src/test/java/io/vertx/rx/java/test/api/ApiTest.java
@@ -19,10 +19,7 @@ import com.acme.rxjava.pkg.sub.SubInterface;
 import io.vertx.rx.java.test.AsyncResultChecker;
 import io.vertx.test.core.TestUtils;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestName;
 import rx.Observable;
 
 import java.util.ArrayList;
@@ -47,14 +44,6 @@ import static org.junit.Assert.assertEquals;
 public class ApiTest {
 
   final TestInterface obj = new TestInterface(new TestInterfaceImpl());
-
-  @Rule
-  public final TestName name = new TestName();
-
-  @Before
-  public void setup() {
-    System.out.println("running " + name.getMethodName());
-  }
 
   @Test
   public void testMethodWithBasicParams() {
