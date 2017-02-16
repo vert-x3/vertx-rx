85e3f2a9ef9015e272fa7ab35f09b1254757b10d
Bjarte Stien Karlsen <bjarte.karlsen@skatteetaten.no>
2017-02-16 20:48:48 +0100

    Add support for custom mapper
     - added a seperate constructor for the UnmarshallingOperator to take a custom mapper.
     - added an aditional helper method for both groovy and java to make it convenient to use
     - added docs for how to use it to unmarshal YAML
    
     Did not do
      - write any tests

--- rx-groovy/src/main/java/io/vertx/rx/groovy/RxHelper.java
+++ rx-groovy/src/main/java/io/vertx/rx/groovy/RxHelper.java
@@ -1,5 +1,6 @@
 package io.vertx.rx.groovy;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.type.TypeReference;
 import io.vertx.core.Context;
 import io.vertx.core.Vertx;
@@ -73,6 +74,29 @@ public class RxHelper {
       @Override
       public io.vertx.core.buffer.Buffer unwrap(Buffer buffer) {
         return buffer;
+      }
+    };
+  }
+
+  /**
+   * Returns a json unmarshaller for the specified java type as a {@link rx.Observable.Operator} instance.<p/>
+   *
+   * The marshaller can be used with the {@link rx.Observable#lift(rx.Observable.Operator)} method to transform
+   * a {@literal Observable<Buffer>} into a {@literal Observable<T>}.<p/>
+   *
+   * The unmarshaller buffers the content until <i>onComplete</i> is called, then unmarshalling happens.<p/>
+   *
+   * Note that the returned observable will emit at most a single object.
+   *
+   * @param mappedType the type to unmarshall
+   * @param mapper the mapper to use to unmarshell
+   * @return the unmarshaller operator
+   */
+  public static <T> Observable.Operator<T, Buffer> unmarshaller(Class<T> mappedType, ObjectMapper mapper) {
+    return new UnmarshallerOperator<T, Buffer>(mappedType, mapper) {
+      @Override
+      public io.vertx.core.buffer.Buffer unwrap(Buffer buffer) {
+        return (buffer);
       }
     };
   }
