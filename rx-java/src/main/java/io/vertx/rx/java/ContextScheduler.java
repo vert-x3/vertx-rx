7423fb21f5778d457a39ffe736aa50f90b41268a
Thomas Segismont <tsegismo@redhat.com>
2017-03-08 17:58:03 +0100

    WorkerExecutorInternal#owner returns a Vert.x instance

--- rx-java/src/main/java/io/vertx/rx/java/ContextScheduler.java
+++ rx-java/src/main/java/io/vertx/rx/java/ContextScheduler.java
@@ -61,7 +61,7 @@ public class ContextScheduler extends Scheduler {
   public ContextScheduler(WorkerExecutor workerExecutor, boolean ordered) {
     this.workerExecutor = () -> workerExecutor;
     this.ordered = ordered;
-    this.vertx = ((WorkerExecutorInternal) workerExecutor).getContext().owner();
+    this.vertx = ((WorkerExecutorInternal) workerExecutor).owner();
     this.context = null;
   }
 
