1a9a0d879e4dc97938c62aafb656284c3f4fa8cc
Julien Viet <julien@julienviet.com>
2018-03-09 18:04:19 +0100

    Make the base stack compile for rx-java2

--- rx-java2/src/test/java/io/vertx/codegen/rxjava2/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
- */
-@ModuleGen(name = "rxjava2", groupPackage = "io.vertx")
-package io.vertx.codegen.rxjava2;
-
-import io.vertx.codegen.annotations.ModuleGen;
