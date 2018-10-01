ba7f08e303a3b61b0f46ee3d2b1ae78eb0f0cd3e
Julien Viet <julien@julienviet.com>
2018-10-01 11:15:35 +0200

    Travis

--- rx-java2-codegen/src/test/java/io/vertx/codegen/extra/UseVertxGenNameDeclarationsWithSameSimpleName.java
+++ rx-java2-codegen/src/test/java/io/vertx/codegen/extra/UseVertxGenNameDeclarationsWithSameSimpleName.java
@@ -13,14 +13,9 @@ public interface UseVertxGenNameDeclarationsWithSameSimpleName extends
   io.vertx.codegen.extra.sub.UseVertxGenNameDeclarationsWithSameSimpleName,
   Handler<io.vertx.codegen.extra.sub.UseVertxGenNameDeclarationsWithSameSimpleName>,
   ReadStream<io.vertx.codegen.extra.sub.UseVertxGenNameDeclarationsWithSameSimpleName> {
-
-  @CacheReturn
-  io.vertx.codegen.extra.sub.UseVertxGenNameDeclarationsWithSameSimpleName foo(io.vertx.codegen.extra.sub.UseVertxGenNameDeclarationsWithSameSimpleName arg);
-
+  @CacheReturn io.vertx.codegen.extra.sub.UseVertxGenNameDeclarationsWithSameSimpleName foo(io.vertx.codegen.extra.sub.UseVertxGenNameDeclarationsWithSameSimpleName arg);
   void function(Function<io.vertx.codegen.extra.sub.UseVertxGenNameDeclarationsWithSameSimpleName, io.vertx.codegen.extra.sub.UseVertxGenNameDeclarationsWithSameSimpleName> function);
   void asyncResult(Handler<AsyncResult<io.vertx.codegen.extra.sub.UseVertxGenNameDeclarationsWithSameSimpleName>> handler);
-
   Handler<io.vertx.codegen.extra.sub.UseVertxGenNameDeclarationsWithSameSimpleName> returnHandler();
   Handler<AsyncResult<io.vertx.codegen.extra.sub.UseVertxGenNameDeclarationsWithSameSimpleName>> returnHandlerAsyncResult();
-
 }
