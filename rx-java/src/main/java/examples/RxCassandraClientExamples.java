492c3bc097d2dffeaa102254407e1b714b5e95f0
Pavel Drankov <titantins@gmail.com>
2018-11-06 15:20:00 +0300

    add 2 more examples

--- rx-java/src/main/java/examples/RxCassandraClientExamples.java
+++ rx-java/src/main/java/examples/RxCassandraClientExamples.java
@@ -1,7 +1,11 @@
 package examples;
 
+import com.datastax.driver.core.Row;
 import io.vertx.rxjava.cassandra.CassandraClient;
+import io.vertx.rxjava.cassandra.CassandraRowStream;
 import io.vertx.rxjava.cassandra.ResultSet;
+
+import java.util.List;
 
 public class RxCassandraClientExamples {
 
@@ -20,5 +24,20 @@ public class RxCassandraClientExamples {
         System.err.println("Whoops, something went wrong...");
         error.printStackTrace();
       });
+  }
+
+  public void simpleQueryStream(CassandraClient cassandraClient) {
+    cassandraClient.rxQueryStream("SELECT my_key FROM my_keyspace.my_table where my_key = my_value ")
+      .subscribe((CassandraRowStream stream) ->
+        stream
+          .handler(row -> System.out.println(row.getString("my_key")))
+          .endHandler(end -> System.out.println("End of stream")));
+  }
+
+  public void simpleFullFetch(CassandraClient cassandraClient) {
+    cassandraClient.rxExecuteWithFullFetch("SELECT my_key FROM my_keyspace.my_table where my_key = my_value ")
+      .subscribe(
+        (List<Row> results) -> results.forEach(row -> System.out.println(row.getString("my_key")))
+      );
   }
 }
