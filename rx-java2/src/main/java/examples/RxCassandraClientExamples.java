737cce692b5065f4b44cd3ee4869c3ecd56c994b
Thomas Segismont <tsegismo@redhat.com>
2018-11-07 17:29:39 +0100

    Improved Cassandra Rx docs

--- rx-java2/src/main/java/examples/RxCassandraClientExamples.java
+++ rx-java2/src/main/java/examples/RxCassandraClientExamples.java
@@ -1,43 +1,47 @@
 package examples;
 
-import com.datastax.driver.core.Row;
+import io.vertx.cassandra.CassandraClientOptions;
 import io.vertx.reactivex.cassandra.CassandraClient;
 import io.vertx.reactivex.cassandra.CassandraRowStream;
-import io.vertx.reactivex.cassandra.ResultSet;
-
-import java.util.List;
+import io.vertx.reactivex.core.Vertx;
 
 public class RxCassandraClientExamples {
 
-  public void connectPrepareExecuteAndDisconnect(CassandraClient cassandraClient) {
-    cassandraClient.rxConnect()
-      .andThen(cassandraClient.rxPrepare("SELECT * FROM my_keyspace.my_table where my_key = ?"))
-      .flatMap(preparedStatement -> cassandraClient.rxExecute(preparedStatement.bind("my_value")))
-      .flatMapMaybe(ResultSet::rxOne)
-      .flatMapCompletable(row -> {
-        // process the row here
-        return cassandraClient.rxDisconnect();
-      })
-      .subscribe(() -> {
-        System.out.println("We are done!");
-      }, error -> {
-        System.err.println("Whoops, something went wrong...");
-        error.printStackTrace();
-      });
+  public void createClient(Vertx vertx) {
+    CassandraClientOptions options = new CassandraClientOptions()
+      .addContactPoint("node1.corp.int")
+      .addContactPoint("node2.corp.int")
+      .addContactPoint("node3.corp.int");
+    CassandraClient cassandraClient = CassandraClient.createShared(vertx, options);
+  }
+
+  public void connect(CassandraClient cassandraClient) {
+    cassandraClient.rxConnect().subscribe(() -> {
+      // Connected succesfully
+    }, throwable -> {
+      // Handle failure
+    });
   }
 
   public void simpleQueryStream(CassandraClient cassandraClient) {
-    cassandraClient.rxQueryStream("SELECT my_key FROM my_keyspace.my_table where my_key = my_value ")
-      .subscribe((CassandraRowStream stream) ->
-        stream
-          .handler(row -> System.out.println(row.getString("my_key")))
-          .endHandler(end -> System.out.println("End of stream")));
+    cassandraClient.rxQueryStream("SELECT my_key FROM my_keyspace.my_table where my_key = my_value")
+      // Convert the stream to a Flowable
+      .flatMapPublisher(CassandraRowStream::toFlowable)
+      .subscribe(row -> {
+        // Handle single row
+      }, t -> {
+        // Handle failure
+      }, () -> {
+        // End of stream
+      });
   }
 
   public void simpleFullFetch(CassandraClient cassandraClient) {
-    cassandraClient.rxExecuteWithFullFetch("SELECT my_key FROM my_keyspace.my_table where my_key = my_value ")
-      .subscribe(
-        (List<Row> results) -> results.forEach(row -> System.out.println(row.getString("my_key")))
-      );
+    cassandraClient.rxExecuteWithFullFetch("SELECT my_key FROM my_keyspace.my_table where my_key = my_value")
+      .subscribe(rows -> {
+        // Handle list of rows
+      }, throwable -> {
+        // Handle failure
+      });
   }
 }
