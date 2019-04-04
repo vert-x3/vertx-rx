1b0c2547436b63e6813a3532eb601cf1791fae6c
slinkydeveloper <francescoguard@gmail.com>
2019-04-04 11:59:56 +0200

    Fixed RxWebApiContractExamples
    Signed-off-by: slinkydeveloper <francescoguard@gmail.com>

--- rx-java-gen/src/test/java/io/vertx/codegen/testmodel/package-info.java
+++ rx-java-gen/src/test/java/io/vertx/codegen/testmodel/package-info.java
@@ -1,7 +1,16 @@
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
-@ModuleGen(name = "testmodel", groupPackage = "io.vertx")
+@ModuleGen(
+  name = "testmodel",
+  groupPackage = "io.vertx",
+  codecs = {
+    MyPojoToJsonArray.MyPojoToJsonArrayCodec.class,
+    MyPojoToJsonObject.MyPojoToJsonObjectCodec.class,
+    MyPojoToInteger.MyPojoToIntegerCodec.class,
+    ZonedDateTimeCodec.class
+  }
+)
 package io.vertx.codegen.testmodel;
 
 import io.vertx.codegen.annotations.ModuleGen;
