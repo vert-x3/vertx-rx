9b6b2bff985c8e7065090eca1baa8d0c7af699bf
Julien Viet <julien@julienviet.com>
2019-05-19 22:52:34 +0200

    Update to use new websocket client methods

--- rx-java2/src/main/java/examples/RxifiedExamples.java
+++ rx-java2/src/main/java/examples/RxifiedExamples.java
@@ -263,7 +263,7 @@ public class RxifiedExamples {
 
   public void websocketClient(Vertx vertx) {
     HttpClient client = vertx.createHttpClient(new HttpClientOptions());
-    client.websocketStream(8080, "localhost", "/the_uri").toObservable().subscribe(
+    client.rxWebSocket(8080, "localhost", "/the_uri").subscribe(
         ws -> {
           // Use the websocket
         },
