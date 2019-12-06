7adef2b2641a73f5adc5b9fc2c13b3c905b7721b
Julien Viet <julien@julienviet.com>
2019-12-06 15:10:14 +0100

    Releasing 4.0.0-SNAPSHOT

--- rx-java2/src/test/java/io/vertx/it/discovery/service/HelloServiceVertxEBProxy.java
+++ rx-java2/src/test/java/io/vertx/it/discovery/service/HelloServiceVertxEBProxy.java
@@ -54,13 +54,14 @@ public class HelloServiceVertxEBProxy implements HelloService {
     this._vertx = vertx;
     this._address = address;
     this._options = options;
-    try{
+    try {
       this._vertx.eventBus().registerDefaultCodec(ServiceException.class, new ServiceExceptionMessageCodec());
-    } catch (IllegalStateException ex) {}
+    } catch (IllegalStateException ex) {
+    }
   }
 
   @Override
-  public  void hello(JsonObject name, Handler<AsyncResult<String>> resultHandler){
+  public void hello(JsonObject name, Handler<AsyncResult<String>> resultHandler){
     if (closed) {
       resultHandler.handle(Future.failedFuture(new IllegalStateException("Proxy is closed")));
       return;
