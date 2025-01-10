8457e1555149411682cc7df843741ebaec9852e0
Thomas Segismont <tsegismont@gmail.com>
2025-01-10 15:50:40 +0100

    Mark Rx types as deprecated if needed
    If the corresponding Vert.x API is deprecated, the Rx API must be deprecated as well.
    
    This will help users upgrade their applications incrementally.
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

---
+++ rx-java2-gen/src/test/java/io/vertx/codegen/extra/DeprecatedType.java
@@ -0,0 +1,11 @@
+package io.vertx.codegen.extra;
+
+import io.vertx.codegen.annotations.VertxGen;
+
+@VertxGen
+@Deprecated
+public interface DeprecatedType {
+
+  void foo();
+
+}
---
+++ rx-java3-gen/src/test/java/io/vertx/rxjava3/test/DeprecationTest.java
@@ -0,0 +1,21 @@
+package io.vertx.rxjava3.test;
+
+import io.vertx.rxjava3.codegen.extra.DeprecatedMethod;
+import io.vertx.rxjava3.codegen.extra.DeprecatedType;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+@SuppressWarnings("deprecation")
+public class DeprecationTest {
+
+  @Test
+  public void testDeprecatedType() {
+    assertNotNull(DeprecatedType.class.getAnnotation(Deprecated.class));
+  }
+
+  @Test
+  public void testDeprecatedMethod() throws Exception {
+    assertNotNull(DeprecatedMethod.class.getMethod("foo").getAnnotation(Deprecated.class));
+  }
+}
